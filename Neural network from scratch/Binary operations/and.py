8de0cbdfe29e51559d04e9c7e29675c4ae32e780
Abin <bintanghidayatullah021@gmail.com>
2025-05-08 22:04:19 +0700

    Update and.py

--- Neural network from scratch/Binary operations/and.py
+++ Neural network from scratch/Binary operations/and.py
@@ -1,9 +1,11 @@
+# activation function
 def sigmoid(z):
     return 1/(1 + 2.71828 ** (-z))
 
 def d_sigmoid(z):
     return sigmoid(z)*(1 - sigmoid(z))
 
+# dataset
 data = [
     [(0, 0), 0],
     [(1, 0), 0],
@@ -13,20 +15,24 @@ data = [
 
 inputs = [a for a, b in data]
 
+# weight and bias
 w_output =[
     [0.5],
     [0.5]
 ]
-
 b_output = 0.5
+
 learn_rate = 0.1
 epoch = 10000
 
 for train in range(epoch):
     for i in range(len(inputs)):
+        # forward pass
         z = sum(a*b for a, b in zip(inputs[i], [w[0] for w in w_output])) + b_output
         
         output = sigmoid(z)
+
+        # backpropagation
         loss = (output - data[i][1]) ** 2
         
         d_loss_output = 2 * (sigmoid(z) - data[i][1])
@@ -38,12 +44,13 @@ for train in range(epoch):
         d_loss_w1 = d_loss_output * d_output_z * d_z_w1
         d_loss_w2 = d_loss_output * d_output_z * d_z_w2
         d_loss_b = d_loss_output * d_output_z * d_z_b
-        
+
+        # update weight and bias
         w_output[0][0] = w_output[0][0] - learn_rate * d_loss_w1
         w_output[1][0] = w_output[1][0] - learn_rate * d_loss_w2
         b_output = b_output - learn_rate * d_loss_b
  
-
+# result
 for i in range(len(inputs)):
         z = sum(a*b for a, b in zip(inputs[i], [w[0] for w in w_output])) + b_output
         
