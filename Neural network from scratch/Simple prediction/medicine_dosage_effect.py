e09b71ba0865e473233797a0edc28353196f8b83
Abin <bintanghidayatullah021@gmail.com>
2025-05-12 07:38:37 +0700

    Update medicine_dosage_effect.py

--- Neural network from scratch/Simple prediction/medicine_dosage_effect.py
+++ Neural network from scratch/Simple prediction/medicine_dosage_effect.py
@@ -34,18 +34,52 @@ b_hidden = [0.1, -0.1]
 w_output = [0.25, -0.25]
 b_output = 0
 
+# learning rate and epoch
+learn_rate = 0.1
+epoch = 10000
+
+for train in range(epoch):
+    for i in range(len(dataset)):
+        # forward pass
+        hidden_raw = [w * dataset[i][0] + bias for w, bias in zip(w_hidden, b_hidden)]
+        hidden_output = [softplus(z) for z in hidden_raw]
+        
+        output_raw = sum([i * w for i, w in zip(hidden_output, w_output)]) + b_output
+        output = softplus(output_raw)
+        
+        # backpropagation
+        loss = (output - dataset[i][1]) ** 2
+        d_loss_output = 2 * (output - dataset[i][1])
+        d_output_Oraw = d_softplus(output_raw)
+        
+        d_loss_Oraw = d_loss_output * d_output_Oraw
+        d_Oraw_hiddenO = w_output
+        d_hiddenO_hiddenR = [d_softplus(z) for z in hidden_raw]
+        
+        d_Oraw_wO = [h for h in hidden_output]
+        d_Oraw_b3 = 1
+        d_hiddenR_wH = dataset[i][0]
+        
+        
+        d_loss_wH = [d_loss_output * d_output_Oraw * d_Oraw_hiddenO[p] * d_hiddenO_hiddenR[p] * d_hiddenR_wH for p in range(2)]
+        d_loss_bH = [d_loss_output * d_output_Oraw * d_Oraw_hiddenO[p] * d_hiddenO_hiddenR[p] * 1 for p in range(2)]
+        
+        d_loss_wO = [d_loss_output * d_output_Oraw * d_Oraw_wO[p] for p in range(2)]
+        d_loss_bO = d_loss_output * d_output_Oraw * d_Oraw_b3
+        
+        # update weight and bias
+        w_hidden = [w_hidden[p] - learn_rate * d_loss_wH[p] for p in range(2)]
+        b_hidden = [b_hidden[p] - learn_rate * d_loss_bH[p] for p in range(2)]
+        
+        w_output = [w_output[p] - learn_rate * d_loss_wO[p] for p in range(2)]
+        b_output -= learn_rate * d_loss_bO
+
+# check
 for i in range(len(dataset)):
-    # forward pass
-    hidden_raw = [w * dataset[i][0] + bias for w, bias in zip(w_hidden, b_hidden)]
-    hidden_output = [softplus(z) for z in hidden_raw]
-    
-    output_raw = sum([i * w for i, w in zip(hidden_output, w_output)]) + b_output
-    output = softplus(output_raw)
-    
-    # backpropagation
-    loss = (output - dataset[i][1]) ** 2
-    d_loss_output = 2 * (output - dataset[i][1])
-    d_output_Oraw = d_softplus(output_raw)
-    
-    d_loss_Oraw = d_loss_output * d_output_Oraw
-    
+        # forward pass
+        hidden_raw = [w * dataset[i][0] + bias for w, bias in zip(w_hidden, b_hidden)]
+        hidden_output = [softplus(z) for z in hidden_raw]
+        
+        output_raw = sum([i * w for i, w in zip(hidden_output, w_output)]) + b_output
+        output = softplus(output_raw)
+        print(f"dosage: {dataset[i][0]},\t effectiveness: {output}")
