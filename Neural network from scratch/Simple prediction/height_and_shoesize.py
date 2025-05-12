20c8721baff5c3854a59a5fc70f935caabb89c33
Abin <bintanghidayatullah021@gmail.com>
2025-05-12 21:33:27 +0700

    Update height_and_shoesize.py

--- Neural network from scratch/Simple prediction/height_and_shoesize.py
+++ Neural network from scratch/Simple prediction/height_and_shoesize.py
@@ -1,14 +1,5 @@
-import math
-
-# softplus for the activation function
-def softplus(x):
-    return max(0, x)
-    
-def d_softplus(x):
-    return 1 if x > 0 else 0
-    
 # dataset
-dataset = [
+raw_dataset = [
     [155, 36],  
     [158, 37],
     [160, 38],
@@ -30,6 +21,8 @@ dataset = [
     [200, 48]   
 ]
 
+dataset = [[raw_dataset[i][0]/200, raw_dataset[i][1]] for i in range(len(raw_dataset))] # normalizing dataset
+
 # weight and bias
 w_hidden = [0.5, -0.5]
 b_hidden = [0.1, -0.1]
@@ -37,34 +30,31 @@ w_output = [0.25, -0.25]
 b_output = 0
 
 # learning rate and epoch
-learn_rate = 0.1
+learn_rate = 0.01
 epoch = 10000
 
 for train in range(epoch):
     for i in range(len(dataset)):
         # forward pass
-        hidden_raw = [w * dataset[i][0] + bias for w, bias in zip(w_hidden, b_hidden)]
-        hidden_output = [softplus(z) for z in hidden_raw]
-        
+        hidden_output = [w * dataset[i][0] + bias for w, bias in zip(w_hidden, b_hidden)]
         output = sum([i * w for i, w in zip(hidden_output, w_output)]) + b_output
         
         # backpropagation
         loss = (output - dataset[i][1]) ** 2
         
-        d_loss_output = 2 * (output - dataset[i][1]) * d_softplus(output_raw)
-        d_Oraw_hiddenO = w_output
-        d_hiddenO_hiddenR = [d_softplus(z) for z in hidden_raw]
+        d_loss_output = 2 * (output - dataset[i][1])
+        d_output_hiddenO = w_output
         
-        d_Oraw_wO = [h for h in hidden_output]
-        d_Oraw_b3 = 1
-        d_hiddenR_wH = dataset[i][0]
+        d_output_wO = [h for h in hidden_output]
+        d_output_b3 = 1
+        d_hiddenO_wH = dataset[i][0]
         
         # partial derivative for each weigh and bias
-        d_loss_wH = [d_loss_Oraw * d_Oraw_hiddenO[p] * d_hiddenO_hiddenR[p] * d_hiddenR_wH for p in range(2)]
-        d_loss_bH = [d_loss_Oraw * d_Oraw_hiddenO[p] * d_hiddenO_hiddenR[p] * 1 for p in range(2)]
+        d_loss_wH = [d_loss_output * d_output_hiddenO[p] * d_hiddenO_wH for p in range(2)]
+        d_loss_bH = [d_loss_output * d_output_hiddenO[p] * 1 for p in range(2)]
         
-        d_loss_wO = [d_loss_Oraw * d_Oraw_wO[p] for p in range(2)]
-        d_loss_bO = d_loss_Oraw * d_Oraw_b3
+        d_loss_wO = [d_loss_output * d_output_wO[p] for p in range(2)]
+        d_loss_bO = d_loss_output * d_output_b3
         
         # update weight and bias
         w_hidden = [w_hidden[p] - learn_rate * d_loss_wH[p] for p in range(2)]
@@ -74,23 +64,7 @@ for train in range(epoch):
         b_output -= learn_rate * d_loss_bO
 
 # check
-
-for i in range(len(dataset)):
-        # forward pass
-        hidden_raw = [w * dataset[i][0] + bias for w, bias in zip(w_hidden, b_hidden)]
-        hidden_output = [softplus(z) for z in hidden_raw]
-        
-        output_raw = sum([i * w for i, w in zip(hidden_output, w_output)]) + b_output
-        output = softplus(output_raw)
-        print(f"height: {dataset[i][0]} \t shoe size: {output}")
-
-
-# dosage = float(input('input a dosage in gram: '))
-# hidden_raw = [w * dosage + bias for w, bias in zip(w_hidden, b_hidden)]
-# hidden_output = [softplus(z) for z in hidden_raw]
-        
-# output_raw = sum([i * w for i, w in zip(hidden_output, w_output)]) + b_output
-# output = softplus(output_raw)
-# percentage = str(round(output * 100, 2)) + '%'
-
-# print(f"the efectiveness is {percentage}")
+height = float(input("Height: "))
+hidden_output = [w * (height/200) + bias for w, bias in zip(w_hidden, b_hidden)]
+output = sum([i * w for i, w in zip(hidden_output, w_output)]) + b_output
+print(f"your shoe size is {output:.1f} (EU)")
